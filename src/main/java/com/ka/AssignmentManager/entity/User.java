d88aef67ddacfde88464f217b2afef9e19135695
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-10 23:39:37 +0530

    two tables created

---
+++ src/main/java/com/ka/AssignmentManager/entity/User.java
@@ -0,0 +1,25 @@
+package com.ka.AssignmentManager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate cohortStartDate;
+    private String username;
+    private String password;
+}
