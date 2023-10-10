101a82b2dd775a4341b3cbacdcfdd225652eefd3
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-10 23:20:04 +0530

    application properties added

---
+++ src/main/java/com/ka/AssignmentManager/AssignmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ka.AssignmentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ka/AssignmentManager/AssignmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ka.AssignmentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
